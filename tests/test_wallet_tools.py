00fc4bee6c8fbbec0949356d37e8430f233de491
Willie Marchetto <wrmarchetto@gmail.com>
2022-09-12 16:07:26 -0500

    Add test stubs for remaining wallet_tools functions

--- tests/test_wallet_tools.py
+++ tests/test_wallet_tools.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import pytest
@@ -80,20 +81,152 @@ import pdb
 
 @wallet_running
 class TestWalletTools:
-    def test_get_settings(self, http_api):
-        settings = http_api.get_settings()
-        assert settings.get("pool_metadata_source")
+    # Wallets tests
+    def test_create_wallet(self, http_api):
+        pytest.skip()
 
-    def test_update_settings(self, http_api):
-        smash_source = "direct"
-        http_api.update_settings(smash_source)
-        new_settings = http_api.get_settings()
-        assert new_settings.get("pool_metadata_source") == smash_source
+    def test_create_wallet_from_key(self, http_api):
+        pytest.skip()
 
+    def test_rename_wallet(self, http_api):
+        pytest.skip()
+
+    def test_update_passphrase(self, http_api):
+        pytest.skip()
+
+    def test_delete_wallet(self, http_api):
+        pytest.skip()
+
+    def test_get_all_wallets(self, http_api):
+        pytest.skip()
+
+    def test_get_wallet(self, http_api):
+        pytest.skip()
+
+    def test_get_wallet_by_name(self, http_api):
+        pytest.skip()
+
+    def test_get_balance(self, http_api):
+        pytest.skip()
+
+    def test_get_utxo_stats(self, http_api):
+        pytest.skip()
+
+    def test_get_utxo_snapshot(self, http_api):
+        pytest.skip()
+
+    # Assets tests
+    def test_get_assets(self, http_api):
+        pytest.skip()
+
+    def test_get_asset(self, http_api):
+        pytest.skip()
+
+    # Addresses tests
+    def test_get_addresses(self, http_api):
+        pytest.skip()
+
+    def test_inspect_address(self, http_api):
+        pytest.skip()
+
+    def test_get_transaction(self, http_api):
+        pytest.skip()
+
+    def test_get_transactions(self, http_api):
+        pytest.skip()
+
+    def test_forget_transaction(self, http_api):
+        pytest.skip()
+
+    def test_confirm_tx(self, http_api):
+        pytest.skip()
+
+    # Transactions tests
+    def test_estimate_tx_fee(self, http_api):
+        pytest.skip()
+
+    def test_send_lovelace(self, http_api):
+        pytest.skip()
+
+    def test_send_ada(self, http_api):
+        pytest.skip()
+
+    def test_send_tokens(self, http_api):
+        pytest.skip()
+
+    def test_send_batch_tx(self, http_api):
+        pytest.skip()
+
+    # Transactions (new) tests
+    def test_construct_transaction(self, http_api):
+        pytest.skip()
+
+    def test_sign_transaction(self, http_api):
+        pytest.skip()
+
+    def test_decode_transaction(self, http_api):
+        pytest.skip()
+
+    def test_submit_transaction(self, http_api):
+        pytest.skip()
+
+    def test_(self, http_api):
+        pytest.skip()
+
+    # Migrations tests
+    def test_create_migration_plan(self, http_api):
+        pytest.skip()
+
+    def test_migrate_wallet(self, http_api):
+        pytest.skip()
+
+    # Stake Pools tests
+    def test_list_stake_keys(self, http_api):
+        pytest.skip()
+
+    def test_list_stake_pools(self, http_api):
+        pytest.skip()
+
+    def test_pool_maintenance_actions(self, http_api):
+        pytest.skip()
+
+    def test_trigger_pool_maintenance(self, http_api):
+        pytest.skip()
+
+    def test_estimate_delegation_fee(self, http_api):
+        pytest.skip()
+
+    def test_join_stake_pool(self, http_api):
+        pytest.skip()
+
+    def test_quit_staking(self, http_api):
+        pytest.skip()
+
+    # Keys tests
+    def test_create_account_public_key(self, http_api):
+        pytest.skip()
+
+    def test_get_account_public_key(self, http_api):
+        pytest.skip()
+
+    def test_get_public_key(self, http_api):
+        pytest.skip()
+
+    def test_create_policy_id(self, http_api):
+        pytest.skip()
+
+    def test_create_policy_key(self, http_api):
+        pytest.skip()
+
+    def test_(self, http_api):
+        pytest.skip()
+
+    # Utils tests
     def test_get_smash_health(self, http_api):
         health = http_api.get_smash_health()
         assert health.get("health")
 
+    # Network tests
     def test_get_network_info(self, http_api):
         info = http_api.get_network_info()
         assert info.get("network_info")
@@ -105,3 +238,19 @@ class TestWalletTools:
     def test_get_network_params(self, http_api):
         params = http_api.get_network_params()
         assert params.get("genesis_block_hash")
+
+    # Settings tests
+    def test_get_settings(self, http_api):
+        settings = http_api.get_settings()
+        assert settings.get("pool_metadata_source")
+
+    def test_update_settings(self, http_api):
+        smash_source = "direct"
+        http_api.update_settings(smash_source)
+        new_settings = http_api.get_settings()
+        assert new_settings.get("pool_metadata_source") == smash_source
+
+    # Node tests
+    def test_get_latest_block_header(self, http_api):
+        block_header = http_api.get_latest_block_header()
+        pytest.skip(reason="This endpoint doesn't exist in the current cardano-wallet release")
